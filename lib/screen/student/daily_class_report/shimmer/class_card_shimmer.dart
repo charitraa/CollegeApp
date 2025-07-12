b92cf04cde73d096c4641ad0f54c37fbcbf79882
Charitra Shrestha <stharabi9862187405@gmail.com>
2025-07-12 15:05:51 +0545

    fix the error

--- lib/screen/student/daily_class_report/shimmer/class_card_shimmer.dart
+++ lib/screen/student/daily_class_report/shimmer/class_card_shimmer.dart
@@ -7,84 +7,87 @@ class ClassCardShimmer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(10),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(8),
-        boxShadow: const [
-          BoxShadow(
-            color: Colors.black12,
-            blurRadius: 6,
-            offset: Offset(0, 3),
-          ),
-        ],
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          // Shimmer for icon
-          Shimmer.fromColors(
-            baseColor: AppColors.primary.withOpacity(0.7),
-            highlightColor: Colors.blue[100]!,
-            period: const Duration(milliseconds: 1200),
-            child: Container(
-              width: 36,
-              height: 36,
-              color: Colors.white,
+    return SizedBox(
+      height: 150,
+      child: Container(
+        padding: const EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(8),
+          boxShadow: const [
+            BoxShadow(
+              color: Colors.black12,
+              blurRadius: 6,
+              offset: Offset(0, 3),
             ),
-          ),
-          const SizedBox(height: 8),
-          // Shimmer for title
-          Shimmer.fromColors(
-            baseColor: Colors.green[100]!,
-            highlightColor: Colors.red[100]!,
-            period: const Duration(milliseconds: 1200),
-            child: Container(
-              width: double.infinity, // Use available width
-              height: 13,
-              color: Colors.white,
+          ],
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // Shimmer for icon
+            Shimmer.fromColors(
+              baseColor: AppColors.primary.withOpacity(0.7),
+              highlightColor: Colors.blue[100]!,
+              period: const Duration(milliseconds: 1200),
+              child: Container(
+                width: 36,
+                height: 36,
+                color: Colors.white,
+              ),
             ),
-          ),
-          const SizedBox(height: 4),
-          // Shimmer for description
-          Shimmer.fromColors(
-            baseColor: Colors.grey[200]!,
-            highlightColor: Colors.grey[50]!,
-            period: const Duration(milliseconds: 1200),
-            child: Container(
-              width: double.infinity,
-              height: 33, // Matches 3 lines of text (11px font)
-              color: Colors.white,
+            const SizedBox(height: 8),
+            // Shimmer for title
+            Shimmer.fromColors(
+              baseColor: Colors.green[100]!,
+              highlightColor: Colors.red[100]!,
+              period: const Duration(milliseconds: 1200),
+              child: Container(
+                width: double.infinity, // Use available width
+                height: 13,
+                color: Colors.white,
+              ),
             ),
-          ),
-          const Spacer(),
-          // Shimmer for publish date
-          Shimmer.fromColors(
-            baseColor: Colors.grey[200]!,
-            highlightColor: Colors.grey[50]!,
-            period: const Duration(milliseconds: 1200),
-            child: Container(
-              width: 80,
-              height: 10,
-              color: Colors.white,
-            ),
-          ),
-          // Shimmer for download icon
-          Align(
-            alignment: Alignment.bottomRight,
-            child: Shimmer.fromColors(
+            const SizedBox(height: 4),
+            // Shimmer for description
+            Shimmer.fromColors(
               baseColor: Colors.grey[200]!,
               highlightColor: Colors.grey[50]!,
               period: const Duration(milliseconds: 1200),
               child: Container(
-                width: 20,
-                height: 20,
+                width: double.infinity,
+                height: 33, // Matches 3 lines of text (11px font)
                 color: Colors.white,
               ),
             ),
-          ),
-        ],
+            const Spacer(),
+            // Shimmer for publish date
+            Shimmer.fromColors(
+              baseColor: Colors.grey[200]!,
+              highlightColor: Colors.grey[50]!,
+              period: const Duration(milliseconds: 1200),
+              child: Container(
+                width: 80,
+                height: 10,
+                color: Colors.white,
+              ),
+            ),
+            // Shimmer for download icon
+            Align(
+              alignment: Alignment.bottomRight,
+              child: Shimmer.fromColors(
+                baseColor: Colors.grey[200]!,
+                highlightColor: Colors.grey[50]!,
+                period: const Duration(milliseconds: 1200),
+                child: Container(
+                  width: 20,
+                  height: 20,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
